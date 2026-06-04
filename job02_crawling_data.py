99bf6abcc8ca8e6f7cb07adfcf0429e3d89e89d8
KSK3939 <rlatjsrb777@gmail.com>
2026-06-04 17:08:30 +0900

    world, it

--- job02_crawling_data.py
+++ job02_crawling_data.py
@@ -10,33 +10,52 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+import datetime
 import time
 
 options = ChromeOptions()
 options.add_argument('lang=ko_KR')
-options.add_argument('headless')
+#options.add_argument('headless')
 
 service = ChromeService(executable_path = ChromeDriverManager().install())
 driver = webdriver.Chrome(service = service, options = options)
 
-url = 'https://news.naver.com/section/100'
-driver.get(url)
-button_xpath = '//*[@id="newsct"]/div[4]/div/div[2]/a'
-for i in range(5):
-    driver.find_element(By.XPATH, button_xpath).click()
-    time.sleep(0.5)
+sections = [['World', '104'], ['IT', '105']]
 
+df_titles = pd.DataFrame(columns = ['titles', 'category'])
+for category, section_num in sections:
+    url = 'https://news.naver.com/section/{}'.format(section_num)
+    driver.get(url)
+    time.sleep(1)
+    button_xpath = '//*[@id="newsct"]/div[4]/div/div[2]/a'
+    for i in range(30):
+        driver.find_element(By.XPATH, button_xpath).click()
+        time.sleep(0.5)
 
 #'//*[@id="newsct"]/div[4]/div/div[1]/div[20]/ul/li[5]/div/div/div[2]/a/strong'
 #'//*[@id="newsct"]/div[4]/div/div[1]/div[20]/ul/li[6]/div/div/div[2]/a/strong'
 #'//*[@id="newsct"]/div[4]/div/div[1]/div[21]/ul/li[2]/div/div/div[2]/a/strong'
 #'//*[@id="newsct"]/div[4]/div/div[1]/div[3]/div/p'
 
-for i in range(1, 5):
-    for j in range(1, 7):
-        try:
-            title_xpath = '//*[@id="newsct"]/div[4]/div/div[1]/div[{}]/ul/li[{}]/div/div/div[2]/a/strong'.format(i, j)
-            title = driver.find_element(By.XPATH, title_xpath).text
-            print(title)
-        except:
-            print('error', i, j)
+    titles = []
+
+    for i in range(1, 188):
+        for j in range(1, 7):
+            try:
+                title_xpath = '//*[@id="newsct"]/div[4]/div/div[1]/div[{}]/ul/li[{}]/div/div/div[2]/a/strong'.format(i, j)
+                title = driver.find_element(By.XPATH, title_xpath).text
+
+                titles.append(title)
+                print(title)
+            except:
+                print('error', i, j)
+
+    titles = list(dict.fromkeys(titles))
+    df_section_titles = pd.DataFrame(titles, columns=['titles'])
+    df_section_titles['category'] = category
+    df_titles = pd.concat([df_titles, df_section_titles], ignore_index=True)
+
+print(df_titles.head())
+df_titles.info()
+df_titles.to_csv('./data/naver_headline_news_{}.csv'.format(datetime.datetime.now().strftime('%Y%m%d')), index = False)
